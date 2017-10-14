f153a6e1ca613c32041a46442b8f021467c68fa8
Emre <eay@kth.se>
2017-10-14 14:29:44 +0100

    Simple script to install some packages I install for every new machine

---
+++ setup-this-machine/setup_this_machine.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+endcol="\033[0m"
+red="\033[031m"
+green="\033[032m"
+yellow="\033[033m"
+purple="\033[035m"
+
+printc () {
+    echo -e $1$2 $endcol    
+}
+
+install () {
+    printc $green "\nProcess: Install $1...\n"
+    sudo apt-get install $1
+}
+
+install git
+install curl
+install trash-cli
+install terminator
+install sqlitebrowser 
+install meld
+install gimp
+install gthumb 
+install unzip
+install htop
+
